1ed1f45a40039b729711b4f81c290b5977387a8f
Christopher Harding <chrisharding88@gmail.com>
2019-09-14 01:04:04 -0400

    created a function on app.js that can display the buttons after the user type in the music artist and hit the Add button

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,51 @@
-$(document).ready(function(){
-    var artist =[];
+$(document).ready(function() {
+    var artistsArr =[];
+    var musicInput;
+    var musicArtist;
+
+
+function applyButtons(){
+
+    $('#button-view').empty();
+
+    for (var i = 0; i < artistsArr.length; i++){
+
+     musicArtist = $('<button>');
+     musicArtist.addClass("music");
+     musicArtist.attr('data-name', artistsArr[i]);
+     musicArtist.html(artistsArr[i]);
+     $('#button-view').append(musicArtist);
+     
+
+    }
+
+    $(".music").css({'margin': '5px'});
+
+
+}
+
+
+
+
+
+$('#addMusicArtist').click(function(){
+    event.preventDefault();
+
+    musicInput = $('#music-input').val().trim();
+
+    artistsArr.push(musicInput);
+
+    applyButtons();
+
+});
+
+
+
+applyButtons();
+
+
+
+
+
 
 });
