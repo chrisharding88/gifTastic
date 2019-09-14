e184e96983601b57247bb16612c4e9d7f56cc690
Christopher Harding <chrisharding88@gmail.com>
2019-09-14 14:12:54 -0400

    created a shortcut inside applyButtons() function plus adding the jQuery function to remove the text after the input gets submitted

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    var artistsArr =[];
+    // Global Variables
+    var artistsArr = [];
     var musicInput;
     var musicArtist;
 
@@ -10,15 +11,15 @@ function applyButtons(){
 
     for (var i = 0; i < artistsArr.length; i++){
 
-     musicArtist = $('<button>');
-     musicArtist.addClass("music");
-     musicArtist.attr('data-name', artistsArr[i]);
-     musicArtist.html(artistsArr[i]);
-     $('#button-view').append(musicArtist);
-     
+     musicArtist = artistsArr[i]; 
 
+     //append the button into the DOM, adds a class "music", and sets attribute to data-name
+     $('#button-view').append(`
+     <button class = "music" data-name = "${musicArtist}">${musicArtist}</button> 
+     `);
     }
 
+    //Each button margin is 5 px
     $(".music").css({'margin': '5px'});
 
 
@@ -28,13 +29,18 @@ function applyButtons(){
 
 
 
-$('#addMusicArtist').click(function(){
+$('#addMusicArtist').click(function(event){
+    //Resets after submit
     event.preventDefault();
 
     musicInput = $('#music-input').val().trim();
 
+    // Clears out the text in the textbox after it's submitted
+    $('#music-input').val("");
+
     artistsArr.push(musicInput);
 
+    // Executes the applyButtons function after it's submitted
     applyButtons();
 
 });
